f1ba984e1a2f3bd7029449f393e40c2113902f0e
Tomo Kaneko <tkaneko@my.bcit.ca>
2021-03-25 20:33:56 -0700

    js file

---
+++ scripts/firebase_team43.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC1XB0QBikZnm5gH176Kf_BWCsOPFLf2sA",
+    authDomain: "stratadeli.firebaseapp.com",
+    projectId: "stratadeli",
+    storageBucket: "stratadeli.appspot.com",
+    messagingSenderId: "32980042067",
+    appId: "1:32980042067:web:cc95843368c4ec87f321a6"
+    };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
