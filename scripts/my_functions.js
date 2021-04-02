c353f81a7c21db39e1bac6b660181a27b48ccc8d
Kerry Chow <kerry_chow@hotmail.ca>
2021-04-02 16:21:58 -0700

    Add updated search bar and CSS +Javascript to search

--- scripts/my_functions.js
+++ scripts/my_functions.js
@@ -1,4 +1,5 @@
 
+// Say Hello to User Upon Signing in
 function sayHello() {
     firebase.auth().onAuthStateChanged(function (somebody) {
         if (somebody) {
@@ -19,6 +20,8 @@ function sayHello() {
 }
 sayHello();
 
+
+// Read database and write items into main.html
 function writeItems() {
     var itemsRef = db.collection('grocery_items')
 
@@ -37,15 +40,34 @@ function writeItems() {
         name: 'meat'
     });
 }
-
+// Read database and display items onto main.html
 function displayItems() {
     db.collection('grocery_items').get()
-    .then(function(snap){
-        snap.forEach(function(doc){
-            var n = doc.data().name;
-            var itemcode = doc.data().code;
-            document.getElementById(itemcode).innerText = n
+        .then(function (snap) {
+            snap.forEach(function (doc) {
+                var n = doc.data().name;
+                var itemcode = doc.data().code;
+                document.getElementById(itemcode).innerText = n
+            })
         })
+}
+
+// Search button event listener
+function getItem() {
+    document.getElementById("submit").addEventListener('click', function () {
+        let item = document.getElementById("search-item").value;
+        console.log(item);
+
+        //read collection in database
+        db.collection("cities")
+            .where("name", "==", location)
+            .get()
+            .then(function (snap) {
+                snap.forEach(function (doc) {
+                    console.log(doc.data());
+                    //Retrieve generated searchresult page
+                })
+            })
     })
 }
-displayItems();
+getItem();
