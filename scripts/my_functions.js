5e7dbe874fceab23d0bb22090d82680562804092
Kerry Chow <kerry_chow@hotmail.ca>
2021-04-15 12:00:12 -0700

    Moved functions in to my_functions.js

--- scripts/my_functions.js
+++ scripts/my_functions.js
@@ -18,28 +18,7 @@ function sayHello() {
     })
 
 }
-sayHello();
 
-
-// Read database and write items into main.html
-function writeItems() {
-    var itemsRef = db.collection('grocery_items')
-
-    itemsRef.add({
-        code: 'VEG',
-        name: 'vegetables'
-    });
-
-    itemsRef.add({
-        code: 'DAI',
-        name: 'dairy'
-    });
-
-    itemsRef.add({
-        code: 'MEAT',
-        name: 'meat'
-    });
-}
 // Read database and display items onto main.html
 function displayItems() {
     db.collection('grocery_items').get()
@@ -69,4 +48,34 @@ function getItem() {
                 })
             })
     })
+}
+
+// Functions for eligible.html form submission
+function gatherValues() {
+    let storeRecipientName = document.getElementById('recipient-name').value;
+    let storeRecipientPhoneNumber = document.getElementById('recipient-phone-number').value;
+    let storeRecipientAddress = document.getElementById('recipient-address').value;
+    let storeRecipientCity = document.getElementById('recipient-city').value;
+    let storeRecipientMessage = document.getElementById('message-text').value;
+    sendRequestToFirebase(storeRecipientName, storeRecipientPhoneNumber, storeRecipientAddress, storeRecipientCity, storeRecipientMessage);
+
+}
+
+function resetValues() {
+    document.getElementById('recipient-name').value = "";
+    document.getElementById('recipient-phone-number').value = "";
+    document.getElementById('recipient-address').value = "";
+    document.getElementById('recipient-city').value = "";
+    document.getElementById('message-text').value = "";
+}
+
+function sendRequestToFirebase(userName, userPhone, userAddress, userCity, userMessage) {
+    var locationRef = db.collection("LocationRequest");
+    locationRef.add({
+        name: userName,
+        phoneNumber: userPhone,
+        address: userAddress,
+        city: userCity,
+        message: userMessage
+    });
 }
