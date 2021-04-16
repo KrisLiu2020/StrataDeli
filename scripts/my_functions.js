31f30160d617dc781ca9fa750aaee301e2a1be4f
Kerry Chow <kerry_chow@hotmail.ca>
2021-04-16 09:41:19 -0700

    Refactor javascript into my_functions.js

--- scripts/my_functions.js
+++ scripts/my_functions.js
@@ -107,6 +107,22 @@ function displayAnswer() {
 
         })
 }
+function showAndHideAccordion(){
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
+  }
 
 
 // Functions for checkout.html to read / check out and write into database
